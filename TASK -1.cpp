30ae440083ce7c83fd812f0c5c86f20e500f778e
abds059 <abdurrehmans059@gmail.com>
2024-04-07 12:36:02 +0500

    Task 1

--- TASK -1.cpp
+++ TASK -1.cpp
@@ -1,7 +1,51 @@
-#include<iostream>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    srand(time(0));
+    int randomno = rand() % 100;
+    int guess;
+    int attemptno = 0;
+    char choice;
+
+    do
+    {
+        do
+        {
+            
+                cout << "Enter your guess: " << endl;
+                cin >> guess;
+
+                if (guess > randomno)
+                {
+                    cout << "Too high! " << endl;
+                }
+
+                if (guess < randomno)
+                {
+                    cout << "Too low! " << endl;
+                }
+
+                if (guess == randomno)
+                {
+                    cout << "Congratulations you guessed it!" << endl;
+                    break;
+                }
+                attemptno++;
+            
+            }while (attemptno < 10);
+        cout << "Play Again" << endl
+             << "('Y'for yes, 'N' for no): "
+             << endl;
+        cin >> choice;
+        attemptno = 0;
+        randomno = rand() % 100;
+
+    } while ( choice != 'N');
+
     return 0;
 }
